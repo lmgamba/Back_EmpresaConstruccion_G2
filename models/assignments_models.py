b28df189faa1cd1080f4195852fb27b5ccf4da05
Laura <72361695+lmgamba@users.noreply.github.com>
2026-02-12 11:06:05 +0100

    Delete models directory

--- models/assignments_models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-from datetime import date
-
-class AssignmentCreate(BaseModel):
-    users_id: int
-    constructionsSites_id: int
-    date_start: date
-    date_finish: Optional[date] = None
